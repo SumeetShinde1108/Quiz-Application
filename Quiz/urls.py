56065c56afcd60bd8e7d24315668e135dddd0439
Sumeet Shinde <166290031+SumeetShinde1108@users.noreply.github.com>
2024-11-21 07:49:15 +0530

    commit #1

---
+++ Quiz/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path, include
+from django.contrib import admin
+from rest_framework.routers import DefaultRouter
+from quiz_app import views
+from rest_framework_simplejwt.views import TokenObtainPairView , TokenRefreshView , TokenVerifyView
+
+router = DefaultRouter()
+
+urlpatterns = [
+
+    path('', include(router.urls), ),
+    path('admin/',admin.site.urls, ),
+
+    path('gettoken/', TokenObtainPairView.as_view(), name='token_obtain_pair', ),
+    path('refreshtoken/', TokenRefreshView.as_view(), name='token_refresh', ),
+    path('verifytoken/', TokenVerifyView.as_view(), name='token_verify', ),
+    
+    path('api/quiz/<int:pk>/', views.QuizDetailView.as_view(), name='quiz-detail', ),
+    path('api/quiz/<int:quiz_id>/leaderboard/', views.LeaderboardView.as_view(), name='quiz-leaderboard', ),
+    path('api/quiz/<int:quiz_id>/attempt/', views.QuizAttemptListView.as_view(), name='quiz-attempt', ),
+    path('api/quiz/create/', views.QuizCreateView.as_view(), name='quiz-create', ),
+
+]
---
+++ quiz_app/serializers.py
@@ -0,0 +1,100 @@
+from rest_framework import serializers
+from .models import Quiz, Answer, Choice, Leaderboard, Question, QuizAttempt 
+
+class ChoiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Choice
+        fields = ['id', 'text', 'is_correct']  
+        read_only_fields = ['is_correct']
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    choices = ChoiceSerializer(many=True, read_only=True)  
+
+    class Meta:
+        model = Question
+        fields = ['id', 'text', 'choices']
+
+
+class LeaderboardSerializer(serializers.ModelSerializer):
+    user_username = serializers.CharField(source='user.username', read_only=True) 
+    quiz_title = serializers.CharField(source='quiz.title', read_only=True)  
+
+    class Meta:
+        model = Leaderboard
+        fields = ['id', 'quiz', 'quiz_title', 'user', 'user_username', 'score', 'rank']
+
+
+class QuizAttemptSerializer(serializers.ModelSerializer):
+    user_username = serializers.CharField(source='user.username', read_only=True)
+    answers = serializers.SerializerMethodField()
+    quiz_name = serializers.CharField(source='quiz.title', read_only=True)
+
+    class Meta:
+        model = QuizAttempt
+        fields = ['id', 'user', 'user_username', 'quiz_name','start_time', 'end_time', 'answers']
+
+    def get_answers(self, obj):
+        answers = obj.answers.all()
+        return [
+            {
+                "question": answer.question.text,
+                "selected_choice": answer.selected_choice.text,
+                "Answer_is": answer.is_correct,
+                "correct_answer": self.get_correct_answer(answer.question)
+            }
+            for answer in answers
+        ]
+
+    def get_correct_answer(self, question):
+        correct_choice = question.choices.filter(is_correct=True).first()
+        return correct_choice.text if correct_choice else None
+
+
+class QuizDetailSerializer(serializers.ModelSerializer):
+    creator_username = serializers.CharField(source='creator.username', read_only=True) 
+    questions = QuestionSerializer(many=True, read_only=True) 
+    leaderboard = serializers.SerializerMethodField()  
+    attempts = QuizAttemptSerializer(many=True, read_only=True)  
+
+    
+    class Meta:
+        model = Quiz
+        fields = [
+            'id', 'title', 'description', 'creator_username', 'start_time', 'end_time', 
+            'is_active', 'questions', 'leaderboard', 'attempts'
+        ]
+
+    def get_leaderboard(self, obj):
+        leaderboard_entries = obj.leaderboard.all().order_by('rank')
+        return [
+            {
+                "user": entry.user.username,
+                "score": entry.score,
+                "rank": entry.rank
+            }
+            for entry in leaderboard_entries
+        ]
+
+
+class QuizCreateSerializer(serializers.ModelSerializer):
+    questions = QuestionSerializer(many=True)
+
+    class Meta:
+        model = Quiz
+        fields = ['id', 'title', 'description', 'start_time', 'end_time', 'is_active', 'questions']
+
+    def create(self, validated_data):
+        questions_data = validated_data.pop('questions')
+        request = self.context.get('request')
+        
+        if request and hasattr(request, 'user'):
+            validated_data['creator'] = request.user
+        
+        quiz = Quiz.objects.create(**validated_data)
+        
+        for question_data in questions_data:
+            question_data['quiz'] = quiz
+            QuestionSerializer().create(question_data)
+        
+        return quiz
