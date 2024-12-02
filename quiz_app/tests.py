035677bb8298d1ce4eba3015c483f63c19ca78eb
Sumeet Shinde <sumeetshinde118@gmail.com>
2024-12-02 20:04:08 +0530

    Added Quiz attempt test case

--- quiz_app/tests.py
+++ quiz_app/tests.py
@@ -2,7 +2,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 from django.test import TestCase
-from .models import Quiz, User
+from .models import Quiz, User, Question, Choice, QuizAttempt
 from django.utils import timezone
 
 
@@ -25,14 +25,28 @@ class QuizApiTestCase(TestCase):
             end_time=timezone.now() + timezone.timedelta(hours=1)
         )
 
+        self.question1 = Question.objects.create(
+            quiz=self.quiz1,
+            text="Sample Question 1?"
+        )
+        self.choice1 = Choice.objects.create(
+            question=self.question1,
+            text="Option 1",
+            is_correct=False
+        )
+        self.choice2 = Choice.objects.create(
+            question=self.question1,
+            text="Option 2",
+            is_correct=True
+        )
 
     def test_get_single_quiz(self):
-        url = reverse('quiz-detail', kwargs={'pk': self.quiz1.pk})  
+        url = reverse('quiz-detail', kwargs={'pk': self.quiz1.pk})
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_get_quiz_leaderboard(self):
-        url = reverse('quiz-leaderboard', kwargs={'quiz_id': self.quiz1.pk})  
+        url = reverse('quiz-leaderboard', kwargs={'quiz_id': self.quiz1.pk})
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
@@ -57,4 +71,24 @@ class QuizApiTestCase(TestCase):
         }
         response = self.client.post(url, payload, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertIn('quiz_id', response.data)  
+        self.assertIn('quiz_id', response.data)
+
+    def test_attempt_quiz(self):
+        self.client.force_authenticate(user=self.user)
+        url = reverse('quiz-attempt-create')
+        payload = {
+            "quiz": self.quiz1.id,
+            "answers": [
+                {
+                    "question": self.quiz1.questions.first().id,
+                    "selected_choice": self.quiz1.questions.first().choices.first().id
+                }
+            ]
+        }
+        response = self.client.post(url, payload, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertIn('quiz_attempt_id', response.data)
+        self.assertEqual(QuizAttempt.objects.count(), 1)
+        quiz_attempt = QuizAttempt.objects.first()
+        self.assertEqual(quiz_attempt.quiz.id, self.quiz1.id)
+        self.assertEqual(quiz_attempt.user.id, self.user.id)
