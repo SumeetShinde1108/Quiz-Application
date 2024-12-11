ff3fc0a84a623eba5e6f188f54370f33f509b77b
Sumeet Shinde <sumeetshinde118@gmail.com>
2024-12-11 11:54:10 +0530

    docker file created.

--- Quiz/settings.py
+++ Quiz/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-!jy5l7f^gdxyf^7(k++(wa5in9yzd$zq8i3ddb(4maa-$h1694
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
