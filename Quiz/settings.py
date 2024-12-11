37011f4c856c4254e7e3d107d55c62824060c24e
Sumeet Shinde <sumeetshinde118@gmail.com>
2024-12-11 16:11:30 +0530

    Updations in docker-compose.yml

--- Quiz/settings.py
+++ Quiz/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'Quiz.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydb',  
+        'USER': 'admin',  
+        'PASSWORD': 'mypassword',  
+        'HOST': 'postgres',  
+        'PORT': '5432',
     }
 }
 
@@ -132,6 +136,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
