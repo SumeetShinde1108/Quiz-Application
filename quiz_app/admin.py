152756ca719ac89eba7d2ea02a981171a5e4e9e6
Sumeet Shinde <sumeetshinde118@gmail.com>
2024-12-12 17:46:06 +0530

    Modifications

--- quiz_app/admin.py
+++ quiz_app/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from quiz_app.models import (
     Quiz, 
     Question, 
@@ -7,8 +6,6 @@ from quiz_app.models import (
     QuizAttempt, 
     AttemptedAnswers
 )
-
-@admin.register(UserAdmin)
 
 
 class ChoiceInline(admin.TabularInline):
